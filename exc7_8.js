ddfa22c7a08d6011c8ac7d61d341f2434a32d75e
marieash1 <139114884+marieash1@users.noreply.github.com>
2023-11-07 20:34:21 +0500

    Update exc7_8.js

--- exc7_8.js
+++ exc7_8.js
@@ -2,15 +2,11 @@
 //addition, subtraction, multiplication, and division each result in the 8
 //Maryam Asghar
 //11/5/2023
-var ad = 2 + 6;
-var su = 108 - 100;
-var mu = 2 * 4;
-var di = 72 / 9;
-console.log("2+6= ".concat(ad));
-console.log("108-100 =".concat(su));
-console.log("2*4= ".concat(mu));
-console.log("72/9= ".concat(di));
-console.log("3+5");
-console.log("100-92");
-console.log("2*4");
-console.log("72/9");
+console.log(2 + 6);
+console.log(108 - 100);
+console.log(2 * 4);
+console.log(72 / 9);
+console.log(3 + 5);
+console.log(100 - 92);
+console.log(2 * 4);
+console.log(72 / 9);
