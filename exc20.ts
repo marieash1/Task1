b004d36e8e5b6e972bc87e8df400d2f9dfdb452f
marieash1 <139114884+marieash1@users.noreply.github.com>
2023-11-07 21:02:14 +0500

    Update exc20.ts

--- exc20.ts
+++ exc20.ts
@@ -1,6 +1,6 @@
 // Q#20 solution
 const lang:string[]=["Urdu","English","French","Spanish","Arabic"];
 console.log("List of languages");
-for(let i:number=1;i<=lang.length;i++){
-    console.log(`${i} . ${lang[i]}`);
+for(let i:number=0;i<lang.length;i++){
+    console.log(`${i+1} . ${lang[i]}`);
 }
