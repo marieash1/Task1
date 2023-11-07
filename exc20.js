09c7be35dd14cf4dacc94ec3312c9ec3c14272a0
marieash1 <139114884+marieash1@users.noreply.github.com>
2023-11-08 00:22:04 +0500

    Update exc20.js

--- exc20.js
+++ exc20.js
@@ -1,6 +1,8 @@
 // Q#20 solution
-var lang = ["Urdu", "English", "French", "Spanish", "Arabic"];
-console.log("List of languages");
-for (var i = 0; i < lang.length; i++) {
-    console.log("".concat(i + 1, " . ").concat(lang[i]));
-}
+var multi = [["Urdu", "English", "French"], ["Mount Everst", "K-2", "Nanga Parbat"]];
+console.log(multi[0][0]);
+console.log(multi[0][1]);
+console.log(multi[0][2]);
+console.log(multi[1][0]);
+console.log(multi[1][1]);
+console.log(multi[1][2]);
