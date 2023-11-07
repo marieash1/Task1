650a13b6d172f21fe182edcbab92b960609b53e3
marieash1 <139114884+marieash1@users.noreply.github.com>
2023-11-07 20:34:00 +0500

    Update exc7_8.ts

--- exc7_8.ts
+++ exc7_8.ts
@@ -2,16 +2,14 @@
 //addition, subtraction, multiplication, and division each result in the 8
 //Maryam Asghar
 //11/5/2023
-let ad:number=2+6;
-let su:number=108-100;
-let mu:number=2*4;
-let di:number=72/9;
-console.log(`2+6= ${ad}`);
-console.log(`108-100 =${su}`);
-console.log(`2*4= ${mu}`);
-console.log(`72/9= ${di}`);
+//7
+console.log(2+6);
+console.log(108-100);
+console.log(2*4);
+console.log(72/9);
 
-console.log("3+5");
-console.log("100-92");
-console.log("2*4");
-console.log("72/9");
+//8
+console.log(3+5);
+console.log(100-92);
+console.log(2*4);
+console.log(72/9);
