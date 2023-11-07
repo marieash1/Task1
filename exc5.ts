883df60efb5ce4a6684d45de86bb27edf6f63cd1
marieash1 <139114884+marieash1@users.noreply.github.com>
2023-11-07 20:33:08 +0500

    Update exc5.ts

--- exc5.ts
+++ exc5.ts
@@ -1,4 +1,5 @@
 // Question 5 solution
 let famous_person:string="Ellen Johnson";
 let message: string="If your dreams don't scare you,they aren't big enough";
-console.log(`${famous_person} once said,"${message}"`);
+let print_message:string= `${famous_person} once said, " ${message} "`;
+console.log(print_message);
