4240032f13ecf726b072331afcbf2781fac5ac04
marieash1 <139114884+marieash1@users.noreply.github.com>
2023-11-07 20:32:36 +0500

    Update exc5.js

--- exc5.js
+++ exc5.js
@@ -1,4 +1,5 @@
 // Question 5 solution
 var famous_person = "Ellen Johnson";
 var message = "If your dreams don't scare you,they aren't big enough";
-console.log("".concat(famous_person, " once said,\"").concat(message, "\""));
+var print_message = "".concat(famous_person, " once said, \" ").concat(message, " \"");
+console.log(print_message);
